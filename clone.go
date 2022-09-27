bab43e7e5c6c14d3eba77f2fb05c5a0092c4bad1
godcong <jumbycc@163.com>
2022-09-27 18:30:26 +0800

    fix(page): update V3

--- clone.go
+++ clone.go
@@ -1,16 +1,20 @@
 package paginator
 
+import (
+	"context"
+)
+
 type wrapClone struct {
 	query  Queryable
 	parser Parser
 }
 
-func (m wrapClone) Count() (int64, error) {
-	return m.query.Finder(m.parser).Count()
+func (m wrapClone) Count(ctx context.Context) (int64, error) {
+	return m.query.Finder(m.parser).Count(ctx)
 }
 
-func (m wrapClone) Get(page PageQuery) (any, error) {
-	return m.query.Finder(m.parser).Get(page)
+func (m wrapClone) Get(ctx context.Context, page PageQuery) (any, error) {
+	return m.query.Finder(m.parser).Get(ctx, page)
 }
 
 func (m wrapClone) Clone() Finder {
